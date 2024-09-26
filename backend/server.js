e80de0a4d5841d55e7993e171694358fabb9de6c
Mohit Mokariya <mokariyamohit123@gmail.com>
2024-09-26 23:11:19 +0530

    Add Swagger Ui

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ const authRoutes = require('./routes/authRoutes');
 const courseRoutes = require('./routes/courseRoutes');
 const enrollmentRoutes = require('./routes/enrollmentRoutes');
 const gradeRoutes = require('./routes/gradeRoutes');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
 
 dotenv.config();
 
@@ -14,6 +17,11 @@ connectDB();
 
 const app = express();
 app.use(express.json());
+
+
+// Load the Swagger YAML file
+const swaggerDocument = YAML.load(path.join(__dirname, 'swagger/swagger.yaml'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/courses', courseRoutes);
